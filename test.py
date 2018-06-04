52abfa655d26e48833e3295c19a51dd9f377133a
Hemanthkaruturi <karuturihemanth@gmail.com>
2018-06-04 18:04:18 +0530

    Third

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import nltk
 import re
 nltk.download('averaged_perceptron_tagger')
 from nltk.tokenize import word_tokenize
-test = "just for testing"
+test = "I am also changing this. Let see what happens"
 text = "Hey i am just testing the git"
 text = str(object=text)
 text = re.sub('[^\w\s]','', text)
