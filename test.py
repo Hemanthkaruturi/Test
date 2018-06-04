a16845cafe6a3a794bff34c6b4be36ffe54a1319
Hemanthkaruturi <karuturihemanth@gmail.com>
2018-06-04 18:01:36 +0530

    In Atom

--- test.py
+++ test.py
@@ -3,10 +3,11 @@ import re
 nltk.download('averaged_perceptron_tagger')
 from nltk.tokenize import word_tokenize
 test = "just for testing"
-text = '''The history of natural-language processing generally started in the 1950s, although work can be found from earlier periods. In 1950, Alan Turing published an article titled "Computing Machinery and Intelligence" which proposed what is now called the Turing test as a criterion of intelligence.The Georgetown experiment in 1954 involved fully automatic translation of more than sixty Russian sentences into English. The authors claimed that within three or five years, machine translation would be a solved problem.[2] However, real progress was much slower, and after the ALPAC report in 1966, which found that ten-year-long research had failed to fulfill the expectations, funding for machine translation was dramatically reduced. Little further research in machine translation was conducted until the late 1980s, when the first statistical machine translation systems were developed.Some notably successful natural-language processing systems developed in the 1960s were SHRDLU, a natural-language system working in restricted "blocks worlds" with restricted vocabularies, and ELIZA, a simulation of a Rogerian psychotherapist, written by Joseph Weizenbaum between 1964 and 1966. Using almost no information about human thought or emotion, ELIZA sometimes provided a startlingly human-like interaction. When the "patient" exceeded the very small knowledge base, ELIZA might provide a generic response, for example, responding to "My head hurts" with "Why do you say your head hurts?".During the 1970s, many programmers began to write "conceptual ontologies", which structured real-world information into computer-understandable data. Examples are MARGIE (Schank, 1975), SAM (Cullingford, 1978), PAM (Wilensky, 1978), TaleSpin (Meehan, 1976), QUALM (Lehnert, 1977), Politics (Carbonell, 1979), and Plot Units (Lehnert 1981). During this time, many chatterbots were written including PARRY, Racter, and Jabberwacky.'''
+text = "Hey i am just testing the git"
 text = str(object=text)
+text = re.sub('[^\w\s]','', text)
 text = word_tokenize(text)
-text = re.sub(r'[^\w\s]', '', text)
+print(text)
 print(text)
 tags = nltk.pos_tag(text)
 print(tags)
